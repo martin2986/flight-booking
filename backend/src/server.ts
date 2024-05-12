982fc710a6bc4752fbb7c40198582f6c2f127aea
martins2986 <m.devit@pm.me>
2024-05-12 20:34:08 +0200

    modified package.json

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,7 +13,7 @@ try {
   console.log(err);
 }
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

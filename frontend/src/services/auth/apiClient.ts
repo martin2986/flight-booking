925e097815e94fa037f5588aa3909fa10092fdb5
martins2986 <m.devit@pm.me>
2024-05-21 10:21:32 +0200

    modifed auth

--- frontend/src/services/auth/apiClient.ts
+++ frontend/src/services/auth/apiClient.ts
@@ -2,18 +2,18 @@ import axios from 'axios';
 import { QueryClient } from '@tanstack/react-query';
 export const queryClient = new QueryClient();
 export const authApi = axios.create({
-  baseURL: `${import.meta.env.VITE_AUTH}`,
+  baseURL: `${import.meta.env.VITE_SECRET_AUTH}`,
   withCredentials: true,
 });
 export const appApi = axios.create({
-  baseURL: `${import.meta.env.VITE_API}`,
+  baseURL: `${import.meta.env.VITE_SECRET_API}`,
   withCredentials: true,
 });
 export const flightClient = axios.create({
-  baseURL: `${import.meta.env.VITE_HOST_BASE_URL}`,
+  baseURL: `${import.meta.env.VITE_SECRET_HOST_BASE_URL}`,
   withCredentials: true,
   headers: {
-    'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
-    'X-RapidAPI-Host': import.meta.env.VITE_HOST,
+    'X-RapidAPI-Key': import.meta.env.VITE_SECRET_API_KEY,
+    'X-RapidAPI-Host': import.meta.env.VITE_SECRET_HOST,
   },
 });
--- frontend/src/utils/firebase.ts
+++ frontend/src/utils/firebase.ts
@@ -1,17 +1,10 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-auth-db741.firebaseapp.com',
-  projectId: 'mern-auth-db741',
-  storageBucket: 'mern-auth-db741.appspot.com',
-  messagingSenderId: '1002597819683',
-  appId: '1:1002597819683:web:0b6373ed58c2ba6f169896',
+  apiKey: import.meta.env.VITE_SECRET_FIREBASE_API,
+  authDomain: import.meta.env.VITE_SECRET_FIRE_DOMAIN,
+  projectId: import.meta.env.VITE_SECRET_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_SECRET_CLOUD_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_SECRET_MESSAGE_ID,
+  appId: import.meta.env.VITE_SECRET_APP_ID,
 };
-
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
